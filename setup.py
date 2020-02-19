59273903d73bba44be0ac6f76db591a0a2e3b574
DSR! <xchwarze@gmail.com>
2020-02-19 02:08:50 -0300

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name='samsungtvws',
-    version='1.3.0',
+    version='1.3.1',
     description='Samsung Smart TV WS API wrapper',
     long_description=readme(),
     long_description_content_type='text/markdown',
