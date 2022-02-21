f43c1d56b3de2a89a438ad354b217d4acbad0e83
epenet <epenet@users.noreply.github.com>
2022-02-21 15:12:09 +0100

    Add tests for invalid app list

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -29,3 +29,15 @@ def test_app_list(connection: Mock) -> None:
             "name": "Deezer",
         },
     ]
+
+
+def test_app_list_invalid(connection: Mock) -> None:
+    """Ensure simple data can be parsed."""
+    open_response = (
+        '{"data": {"token": 123456789}, "event": "ms.channel.connect", "from": "host"}'
+    )
+    app_list_response = '{"data": 200, "event": "ed.apps.launch", "from": "host"}'
+
+    connection.recv.side_effect = [open_response, app_list_response]
+    tv = SamsungTVWS("127.0.0.1")
+    assert tv.app_list() == 200
