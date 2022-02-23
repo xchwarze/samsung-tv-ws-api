360ac91f6b1c84e156a0d380a45477538cedfdeb
epenet <epenet@users.noreply.github.com>
2022-02-23 11:56:34 +0100

    Move REST functionnality to separate class

--- samsungtvws/remote.py
+++ samsungtvws/remote.py
@@ -23,16 +23,13 @@ import logging
 import time
 from typing import overload
 
-import requests
-
-from . import art, connection, exceptions, helper, shortcuts
+from . import art, connection, helper, rest, shortcuts
 
 _LOGGING = logging.getLogger(__name__)
 
 
 class SamsungTVWS(connection.SamsungTVWSConnection):
     _REMOTE_ENDPOINT = "samsung.remote.control"
-    _REST_URL_FORMAT = "{protocol}://{host}:{port}/api/v2/{route}"
 
     def __init__(
         self,
@@ -54,35 +51,10 @@ class SamsungTVWS(connection.SamsungTVWSConnection):
             key_press_delay=key_press_delay,
             name=name,
         )
-
-    def _format_rest_url(self, route=""):
-        params = {
-            "protocol": "https" if self._is_ssl_connection() else "http",
-            "host": self.host,
-            "port": self.port,
-            "route": route,
-        }
-
-        return self._REST_URL_FORMAT.format(**params)
+        self._rest_api = None
 
     def _ws_send(self, command, key_press_delay=None):
         return super().send_command(command, key_press_delay)
-
-    def _rest_request(self, target, method="GET"):
-        url = self._format_rest_url(target)
-        try:
-            if method == "POST":
-                return requests.post(url, timeout=self.timeout, verify=False)
-            elif method == "PUT":
-                return requests.put(url, timeout=self.timeout, verify=False)
-            elif method == "DELETE":
-                return requests.delete(url, timeout=self.timeout, verify=False)
-            else:
-                return requests.get(url, timeout=self.timeout, verify=False)
-        except requests.ConnectionError:
-            raise exceptions.HttpApiError(
-                "TV unreachable or feature not supported on this model."
-            )
 
     # endpoint is kept here for compatibility - can be removed in v2
     @overload
@@ -173,35 +145,25 @@ class SamsungTVWS(connection.SamsungTVWSConnection):
         else:
             return response
 
-    def rest_device_info(self):
-        _LOGGING.debug("Get device info via rest api")
-        response = self._rest_request("")
+    def _get_rest_api(self):
+        if self._rest_api is None:
+            self._rest_api = rest.SamsungTVRest()
+        return self._rest_api
 
-        return helper.process_api_response(response.text)
+    def rest_device_info(self):
+        return self._get_rest_api().rest_device_info()
 
     def rest_app_status(self, app_id):
-        _LOGGING.debug("Get app %s status via rest api", app_id)
-        response = self._rest_request("applications/" + app_id)
-
-        return helper.process_api_response(response.text)
+        return self._get_rest_api().rest_app_status(app_id)
 
     def rest_app_run(self, app_id):
-        _LOGGING.debug("Run app %s via rest api", app_id)
-        response = self._rest_request("applications/" + app_id, "POST")
-
-        return helper.process_api_response(response.text)
+        return self._get_rest_api().rest_app_run(app_id)
 
     def rest_app_close(self, app_id):
-        _LOGGING.debug("Close app %s via rest api", app_id)
-        response = self._rest_request("applications/" + app_id, "DELETE")
-
-        return helper.process_api_response(response.text)
+        return self._get_rest_api().rest_app_close(app_id)
 
     def rest_app_install(self, app_id):
-        _LOGGING.debug("Install app %s via rest api", app_id)
-        response = self._rest_request("applications/" + app_id, "PUT")
-
-        return helper.process_api_response(response.text)
+        return self._get_rest_api().rest_app_install(app_id)
 
     def shortcuts(self):
         return shortcuts.SamsungTVShortcuts(self)
