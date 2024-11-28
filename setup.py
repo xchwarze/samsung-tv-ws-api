1f12ff48e3033e63a217e4f86bd8607cd07a6bda
epenet <6771947+epenet@users.noreply.github.com>
2024-11-28 08:02:52 +0100

    Bump version to 2.7.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name="samsungtvws",
-    version="2.7.0",
+    version="2.7.1",
     description="Samsung Smart TV WS API wrapper",
     long_description=readme(),
     long_description_content_type="text/markdown",
